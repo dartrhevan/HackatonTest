441b6e11d9eb934bcf9bb92b1ddeb5c12b03ecfa
dartrhevan <vovasatunkin@mail.ru>
2019-10-11 14:48:55 +0500

    handleClick added

--- spa/src/components/Main.js
+++ spa/src/components/Main.js
@@ -9,8 +9,15 @@ class Main extends React.Component {
         super();
 		this.disk = new Disk();
         this.state = {
+            path: this.disk.path,
             data: this.disk.requestData().items
         }
+    }
+
+    handleClick(item) {
+        /*this.state.path = addPath(this.state.path, item.name);
+        this.state.data = this.disk.requestData().items;*/
+        this.setState({path: addPath(this.state.path, item.name), data: this.disk.requestData().items});
     }
 
     render() {
