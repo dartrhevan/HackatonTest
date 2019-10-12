981b6e2c70a8737c1d9362ce7a74b23ff0bf5059
Alexander <AD.Popov.IT@gmail.com>
2019-10-12 21:50:29 +0500

    add icon

--- spa/src/index.js
+++ spa/src/index.js
@@ -4,6 +4,7 @@ import './css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css' 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
